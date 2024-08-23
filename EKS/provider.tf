b9c5817f342b3ca7745853a4c5549eeb4ac1b78f
DevOps Basic <56022440+niranjansinha4u@users.noreply.github.com>
2024-08-23 18:15:29 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,8 +1,4 @@
 provider "aws" {
   region = "us-east-1"
 }
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
+
